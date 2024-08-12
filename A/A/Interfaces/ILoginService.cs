1e958961046fb32b862e9d0acd93e4fbfac0188a
unknown <sbacharii@gmail.com>
2024-08-12 17:05:13 +0200

    csproj

--- A/A/Interfaces/ILoginService.cs
+++ A/A/Interfaces/ILoginService.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static A.Enums.Enums;
 
 namespace A.Interfaces;
 
 public interface ILoginService
 {
     Task<(UserLoginDataDTO UserInfo, ExceptionHandler? Exception)> LoginGeneric(string email, string passWord);
+    Task<(UserLoginDataDTO UserInfo, ExceptionHandler? Exception)> LoginWithAuthProvider(AuthProvider provider);
 }
