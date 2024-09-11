1c8f02e33ef353a6089c645a4e0e61328e86684d
unknown <sbacharii@gmail.com>
2024-09-11 17:36:24 +0200

    zmeny v registraci firmy

--- A/A/Views/Register/RegisterCompanyView.xaml.cs
+++ A/A/Views/Register/RegisterCompanyView.xaml.cs
@@ -1,68 +1,149 @@
-namespace A.Views;
+using System.Globalization;
 
-
-[QueryProperty(nameof(GenericRegistration), "genericRegistration")]
-public partial class RegisterCompanyView : ContentPage
+namespace A.Views
 {
-    int _viewIndex = 0;
-    ImageSource? _imageSource { get; set; } = null;
-    bool _genericRegistration { get; set; } = false;
 
-    public bool GenericRegistration
+    [QueryProperty(nameof(GenericRegistration), "genericRegistration")]
+    public partial class RegisterCompanyView : ContentPage
     {
-        set
+        private int _viewIndex = 0;
+        public int ViewIndex
         {
-            _genericRegistration = value;
-        }
-    }
-
-    public RegisterCompanyView()
-    {
-        InitializeComponent();
-    }
-
-    //https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/storage/file-picker?view=net-maui-8.0&tabs=android
-    public async Task<(FileResult?, string)> PickAndShow(PickOptions options)
-    {
-        FileResult? result = null;
-        string msg = string.Empty;
-
-        try
-        {
-            result = await FilePicker.Default.PickAsync(options);
-
-            if (result != null)
+            get => _viewIndex;
+            set
             {
-                if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
-                    result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
-                {
-                    using var stream = await result.OpenReadAsync();
-                    _imageSource = ImageSource.FromStream(() => stream);
-
-                    this.ImgCompanyLogo.Source = _imageSource;
-                }
+                _viewIndex = value;
+                OnPropertyChanged(nameof(ViewIndex)); // Notify the UI about the change
             }
         }
-        catch (Exception ex)
+
+        ImageSource? _imageSource { get; set; } = null;
+        bool _genericRegistration { get; set; } = false;
+
+        Grid[] _arrGridViews = new Grid[3];
+
+        public bool GenericRegistration
         {
-            msg += ex.Message;
+            set
+            {
+                _genericRegistration = value;
+            }
         }
 
-        return (result, msg);
-    }
+        public RegisterCompanyView()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
 
-    private async void BtnChooseImage_Clicked(object sender, EventArgs e)
-    {
-        await PickAndShow(PickOptions.Images);
-    }
+            //_arrGridViews[0] = this.FirstStepView;
+            //_arrGridViews[1] = this.SecondStepView;
+            //_arrGridViews[2] = this.ThirdStepView;
 
-    private void BtnNext_Clicked(object sender, EventArgs e)
-    {
-        _viewIndex++;
-    }
+            //_arrGridViews[_viewIndex].IsVisible = true;
 
-    private void BtnPrev_Clicked(object sender, EventArgs e)
+            if (this._genericRegistration)
+            {
+                this.lblTitleViewStep.Text = "1/3";
+            }
+            else
+            {
+                this.lblTitleViewStep.Text = "1/2";
+            }
+
+        }
+
+        //https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/storage/file-picker?view=net-maui-8.0&tabs=android
+        public async Task<(FileResult?, string)> PickAndShow(PickOptions options)
+        {
+            FileResult? result = null;
+            string msg = string.Empty;
+
+            try
+            {
+                result = await FilePicker.Default.PickAsync(options);
+
+                if (result != null)
+                {
+                    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        using var stream = await result.OpenReadAsync();
+                        _imageSource = ImageSource.FromStream(() => stream);
+
+                        this.ImgCompanyLogo.Source = _imageSource;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                msg += ex.Message;
+            }
+
+            return (result, msg);
+        }
+
+        private async void BtnChooseImage_Clicked(object sender, EventArgs e)
+        {
+            await PickAndShow(PickOptions.Images);
+        }
+
+        private void BtnNext_Clicked(object sender, EventArgs e)
+        {
+            if (_viewIndex < 2) // Prevent going beyond available views
+            {
+                ViewIndex++;
+                lblTitleViewStep.Text = $"{ViewIndex + 1}/{(_genericRegistration ? 3 : 2)}";
+            }
+        }
+
+        private void BtnPrev_Clicked(object sender, EventArgs e)
+        {
+            if (_viewIndex > 0) // Prevent negative index
+            {
+                ViewIndex--;
+                lblTitleViewStep.Text = $"{ViewIndex + 1}/{(_genericRegistration ? 3 : 2)}";
+            }
+        }
+
+        private void BtnRegister_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EntryEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void EntryPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void EntryPasswordConfirm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
+namespace A.Converters
+{
+    public class StepVisibilityConverter : IValueConverter
     {
-        _viewIndex--;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int viewIndex && parameter is string gridIndex)
+            {
+                // Parse the gridIndex passed as a parameter in XAML and compare it with _viewIndex
+                return viewIndex == int.Parse(gridIndex);
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
